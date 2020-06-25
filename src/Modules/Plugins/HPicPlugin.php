b3f39236449ca48ba660b00b11a869a5d83dddad
Tamce <tamce@outlook.com>
2020-06-25 17:26:44 +0800

    add hpic query

--- src/Modules/Plugins/HPicPlugin.php
+++ src/Modules/Plugins/HPicPlugin.php
@@ -10,11 +10,27 @@ use PHPHtmlParser\Dom;
 class HPicPlugin extends OnMessagePlugin
 {
     protected $commands = [
+        '涩图有多少' => 'query',
+        '涩图 (.*)' => 'query',
         '色图' => 'getPic',
         '瑟图' => 'getPic',
         '涩图' => 'getPic',
     ];
     protected $listen = '*';
+
+    public function query(CQEvent $e, $id = null)
+    {
+        $list = json_decode(file_get_contents(storage('/hpic.json')));
+        if (is_null($id)) {
+            $e->reply('目前涩图存量: '.count($list));
+        } else {
+            if ($id >= count($list) or $id < 0) {
+                $e->reply('合法 id 范围为 0 - '.count($list) - 1);
+            }
+            $url = $list[$id];
+            $e->reply("id: $id\nurl: $url\n".CQCode::image($url));
+        }
+    }
 
     public function getPic(CQEvent $e)
     {
