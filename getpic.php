e3a26e58238ded14992430b895e74c57801d7895
Tamce <tamce@outlook.com>
2020-06-25 16:50:09 +0800

    enhance getpic script

--- getpic.php
+++ getpic.php
@@ -6,16 +6,29 @@ use PHPHtmlParser\Dom;
 require 'src/bootstrap.php';
 
 $client = new Client;
-$response = $client->get('https://yande.re/post?tags=princess_connect', [
-    'timeout' => 5,
-]);
-$dom = new Dom;
-$dom->load($response->getBody()->getContents());
+
 $result = [];
-foreach ($dom->find('a.thumb img') as $item) {
-    $tag = $item->getTag();
-    $url = $tag->getAttribute('src')['value'];
-    $result[] = $url;
+for ($page = 1; $page <= 40; ++$page)
+{
+    $response = $client->get('https://yande.re/post?tags=princess_connect&page='.$page, [
+        'timeout' => 5,
+    ]);
+    $dom = new Dom;
+    $dom->load($response->getBody()->getContents());
+    $items = $dom->find('a.thumb')->toArray();
+    $i = 1;
+    $pageCnt = count($items);
+    foreach ($items as $item) {
+        echo "\rPage $page / 40: \t$i / $pageCnt      ";
+        $i++;
+        $tag = $item->getTag();
+        $url = $tag->getAttribute('href')['value'];
+        $response = $client->get('https://yande.re'.$url);
+        $dom->load($response->getBody()->getContents());
+        $url = $dom->find('#image')[0]->getTag()->getAttribute('src')['value'];
+        $result[] = $url;
+    }
+    echo "\n";
 }
 file_put_contents(storage('/hpic.json'), json_encode($result));
 $count = count($result);
