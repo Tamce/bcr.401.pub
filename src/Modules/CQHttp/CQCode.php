f2c29576522a8abe79de3b574f06d9b79d6dbe96
Tamce <tamce@outlook.com>
2020-06-27 00:49:28 +0800

    misc: try fucking convert images

--- src/Modules/CQHttp/CQCode.php
+++ src/Modules/CQHttp/CQCode.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Modules\CQHttp;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class CQCode
 {
     static public function image($url)
     {
+        return "[CQ:image,file=$url]";
+        if (strlen($url) > 10 and substr($url, 0, 10) == 'downloaded') {
+            $img = Image::make(storage('/image/'.$url));
+            // $img->resize($img->width()*0.5, $img->height()*0.5);
+            $ext = 'jpg';
+            $img->encode($ext)->save(storage("/image/temp.$ext"));
+            return "$url [CQ:image,file=temp.$ext]";
+        }
         return "[CQ:image,file=$url]";
     }
 
