751395aada392874a2519f71ea95c9869e2a13fd
Tamce <tamce@outlook.com>
2020-08-16 23:14:13 +0800

    misc: update deploy script, remove hooks config from cvs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [[ $# != 1 ]]
+then
+    echo "Error: please specify remote branch"
+    exit 1
+fi
 
 git fetch
-git reset --hard origin/master
+git reset --hard origin/$1
 composer install --no-dev -a
