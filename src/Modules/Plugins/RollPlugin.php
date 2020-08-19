1f69361aa0a21873504b17a30fc2cef7d82267be
rittachan <curisundere@gmail.com>
2020-08-20 01:04:37 +0800

    RollPlugin: bugfix

--- src/Modules/Plugins/RollPlugin.php
+++ src/Modules/Plugins/RollPlugin.php
@@ -17,7 +17,7 @@ class RollPlugin extends OnMessagePlugin
         if (!is_numeric($text)) {
             return $e->reply('点数不是一个合法的数字！');
         }
-        $e->reply($e->getSenderId . '摇到的点数是：' . rand(0, $text));
+        $e->reply($e->getSenderId() . '摇到的点数是：' . rand(0, $text));
     }
 
     public function roll(CQMessageEvent $e)
