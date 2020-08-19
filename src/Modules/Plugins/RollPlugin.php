c284b51c82f2b28f3c6a71049db144e6f729c9ba
Tamce <tamce@outlook.com>
2020-08-20 01:24:54 +0800

    roll: move random pick from roll to random

--- src/Modules/Plugins/RollPlugin.php
+++ src/Modules/Plugins/RollPlugin.php
@@ -9,19 +9,25 @@ class RollPlugin extends OnMessagePlugin
     protected $commands = [
         'roll (.*)' => 'rollNum',
         'roll' => 'roll',
+        'random (.*)' => 'random',
     ];
     protected $listen = '*';
 
-    public function rollNum(CQMessageEvent $e, $text)
+    public function random(CQMessageEvent $e, $text)
     {
-        if (is_numeric($text)) {
-            return $e->reply($e->getSenderId() . '摇到的点数是：' . rand(0, $text));
-        }
         $list = collect(explode(' ', $text));
         if ($list->count() == 1) {
-            return $e->reply('点数不是一个合法的数字！');
+            return $e->reply('就一个你还 rand？？？');
         }
         return $e->reply($e->getSenderId() . '摇到了：' . $list->random());
+    }
+
+    public function rollNum(CQMessageEvent $e, $text)
+    {
+        if (!is_integer($text)) {
+            return $e->reply('点数不是一个合法的整数！');
+        }
+        return $e->reply($e->getSenderId() . '摇到的点数是：' . rand(0, $text));
     }
 
     public function roll(CQMessageEvent $e)
