df6620f624c24b441ce09d99a4f42f3fbb0384d1
Tamce <tamce@outlook.com>
2020-08-25 00:31:41 +0800

    gugua: add restriction for toad to trigger

--- src/Modules/Plugins/GuGuaPlugin.php
+++ src/Modules/Plugins/GuGuaPlugin.php
@@ -8,7 +8,7 @@ use App\Modules\CQHttp\CQHttp;
 class GuGuaPlugin extends OnMessagePlugin
 {
     protected $commands = [
-        '^.*七夕.*' => 'toad',
+        '^(.*)' => 'toad',
         '^.*\[CQ:at,qq=2789594061\].*什么功能.*' => 'gugua',
         '^.*\[CQ:at,qq=2789594061\].*能干啥.*' => 'gugua',
         '^.*\[CQ:at,qq=2789594061\].*test.*' => 'test',
@@ -20,13 +20,23 @@ class GuGuaPlugin extends OnMessagePlugin
         $e->reply('test');
     }
 
-    public function toad(CQMessageEvent $e)
+    public function toad(CQMessageEvent $e, $text)
     {
+        $counter = 0;
+        foreach (['七夕', '孤寡', '机器人', '蛤蟆'] as $val) {
+            if (strpos($text, $val) !== false)
+                $counter++;
+        }
+        if ($counter < 2)
+            return;
+
+        app('logger')->info('七夕蛤蟆登场！', [$e->getMessageType() => $e->getMessageSourceId()]);
         return $e->reply('大家好，我是你们朋友点的七夕蛤蟆');
     }
 
     public function gugua(CQMessageEvent $e)
     {
+        app('logger')->info('我要开始叫了！', [$e->getMessageType() => $e->getMessageSourceId()]);
         $cq = CQHttp::instance();
         sleep(2);
         $cq->sendMessage($e->getMessageType(), $e->getMessageSourceId(), '我要开始叫了');
